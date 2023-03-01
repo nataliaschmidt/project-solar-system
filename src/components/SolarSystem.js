a63089f129f08f439846789bc515a6429cb6bef5
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-03-01 16:42:16 -0300

    requisito 6

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
+    // console.log(planets);
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        <ul>
+          {
+            planets
+              .map((planet) => (<PlanetCard
+                key={ planet.name }
+                planetName={ planet.name }
+                planetImage={ planet.image }
+              />))
+          }
+        </ul>
       </div>
     );
   }
