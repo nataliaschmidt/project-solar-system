604e28749019a6d7b68cf7db155296fa3416f80d
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-03-01 16:52:20 -0300

    Requisito 08

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Title from './Title';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
-        <p>Paragrafo teste missions</p>
+        <Title headline="Missões" />
       </div>
     );
   }
